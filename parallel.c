897cc6c2f4191a12829e6412f1302ec5623965e3
Tobias Fraunberger <31957083+Elias278448935@users.noreply.github.com>
2018-12-16 17:06:28 +0100

    Add files via upload

---
+++ parallel.c
@@ -0,0 +1,33 @@
+// habe es komplet neu gemacht
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+char* arguments[] = {"ls", "df", "ps", "pwd"};
+int status;
+
+int main(int argc, char const *argv[])
+{
+  int count = 0;
+  while(count != 4)
+  {
+    char* current[] = {arguments[count], arguments[4]};
+    pid_t pid_ls = fork();
+    if (pid_ls == 0)
+    {
+      execvp(current[0], current);
+
+      printf("ERROR\n");
+    }
+    else
+    {
+        waitpid(-1, &status, 0);
+    }
+    count++;
+  }
+}
+  printf("Done\n");
+
+  return 0;
+}
